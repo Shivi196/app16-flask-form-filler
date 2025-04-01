17a9540216f7c32e1f859a5f23bc2d12ba5a3640
misha <misha07.sharma@gmail.com>
2025-04-01 19:17:02 +0530

    7th commit: Showing Submission Notification

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+
 app = Flask(__name__)
 
+# Before creating Database, Configuring App
 app.config["SECRET_KEY"] = "my application123"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite3:///data.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
 
 db = SQLAlchemy(app)
 
+# creating table form with below cols
 class Form(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     first_name = db.Column(db.String(60))
@@ -30,12 +33,17 @@ def index():
          occupation = request.form["occupation"]
 
          form = Form(first_name=first_name,last_name=last_name,email=email,date=date_obj,occupation=occupation)
+         # for inserting data into db
          db.session.add(form)
          db.session.commit()
+
+         flash(f"{first_name} , Your details submitted successfully!","success")
 
      return render_template("index.html")
 
 if __name__ == "__main__":
+    # for creating database
     with app.app_context():
         db.create_all()
+    # for running  over all app
     app.run(port=5000,debug=True)
