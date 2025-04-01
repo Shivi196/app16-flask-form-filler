f2da55c160a9bce9e489ab5deebf666abb1d0176
misha <misha07.sharma@gmail.com>
2025-04-01 20:47:00 +0530

    8th commit: Sending a Confirmation mail

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request,flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from flask_mail import Mail,Message
 
 app = Flask(__name__)
 
-# Before creating Database, Configuring App
+# App Configurations
 app.config["SECRET_KEY"] = "my application123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
+# Flask Mail Configurations
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "sharmabruno310@gmail.com"
+app.config["MAIL_PASSWORD"] = "retb yloj vqgn gpag"
+
 
 db = SQLAlchemy(app)
+mail = Mail(app)
 
-# creating table form with below cols
+# Defining Form Model (creating table form with below cols)
 class Form(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     first_name = db.Column(db.String(60))
@@ -32,11 +40,23 @@ def index():
          date_obj = datetime.strptime(date,"%Y-%m-%d")
          occupation = request.form["occupation"]
 
+         # Insert form data into the database
          form = Form(first_name=first_name,last_name=last_name,email=email,date=date_obj,occupation=occupation)
-         # for inserting data into db
          db.session.add(form)
          db.session.commit()
 
+         # Create email content
+         message_body = f"Thank you for your submission!! {first_name} \n " \
+                        f"Here are your details : \n" \
+                        f"{first_name} {last_name} \n {email} \n {date}\n {occupation}\n" \
+                        f"Take Care!!"
+
+         # Sending the email
+         msg = Message(subject="New Form submitted!!",
+                       sender = app.config["MAIL_USERNAME"],
+                       recipients=[email],
+                       body=message_body)
+         mail.send(msg)
          flash(f"{first_name} , Your details submitted successfully!","success")
 
      return render_template("index.html")
