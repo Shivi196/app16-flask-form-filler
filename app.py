0f34c6eb40f2535a6462d3aef51166ee826808af
misha <misha07.sharma@gmail.com>
2025-04-01 13:59:19 +0530

    5th commit: Creating Database

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "my application123"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite3:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+
+db = SQLAlchemy(app)
+
+class Form(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    first_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60))
+    email = db.Column(db.String(60))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(60))
 
 @app.route('/',methods=["GET","POST"])
 def index():
@@ -13,4 +29,7 @@ def index():
 
      return render_template("index.html")
 
-app.run(port=5000,debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(port=5000,debug=True)
